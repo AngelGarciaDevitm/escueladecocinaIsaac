f0e38a81543f477e88b1209406904531925a4674
Angel Isaac <angel.garcia@devitm.com>
2025-06-04 12:31:21 -0600

    39- Creando el footer y cargando los scripts

--- functions.php
+++ functions.php
@@ -8,5 +8,10 @@ function edc_scripts() {
     wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.css', false, '4.1.3');
 
     wp_enqueue_style('style', get_stylesheet_uri(), array('bootstrap-css'));
+
+    /*Scripts*/
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('popper', get_template_directory_uri() . '/js/popper.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.js', array('popper'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'edc_scripts' );
--- header.php
+++ header.php
@@ -5,3 +5,32 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <?php wp_head(); ?>
     <body>
+
+    
+    <header class="header py-5 encabezado">
+            <div class="container">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-md-4 col-8 mb-4 mb-md-0">
+                    <a href="index.html">
+                        <img src="img/logo.svg" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col-md-8">
+                    <nav class="navbar navbar-expand-md navbar-light justify-content-center">
+                        <button class="navbar-toggler mb-4" data-toggle="collapse" data-target="#nav_principal" aria-expanded="false" type="button">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+
+                        <div id="nav_principal" class="collapse navbar-collapse justify-content-center justify-content-lg-end text-center text-uppercase">
+                            <a href="nosotros.html" class="nav-link">Nosotros</a>
+                            <a href="blog.html" class="nav-link">Blog</a>
+                            <a href="clases.html" class="nav-link active">Clases</a>
+                            <a href="galeria.html" class="nav-link">Galería</a>
+                            <a href="contacto.html" class="nav-link">Contacto</a>
+                        </div> <!--#nav-principal-->
+                    </nav>
+                </div> <!--.col-md-8-->
+            </div> <!--.row-->
+        </div> <!--container-->
+    </header>
