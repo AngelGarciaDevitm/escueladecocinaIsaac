9b7d04a8306dc20096a5a4c9b222a787f8a7b860
Angel Isaac Garcia <angel.garcia@devitm.com>
2025-07-04 14:44:15 -0600

    71-Agregando el template de comentarios

---
+++ comments.php
@@ -0,0 +1,1 @@
+<h1> hola desde comments </h1>
--- single.php
+++ single.php
@@ -3,8 +3,19 @@
     <?php while(have_posts()): the_post();?>
 
 
-        <?php get_template_part('template-parts/contenido', 'posts');
+        <?php get_template_part('template-parts/contenido', 'posts'); ?>
+
+        <div class="comentarios container">
+            <?php
+                if( comments_open() || get_comments_number() ):
+                    comments_template();
+                else:
+                    echo "<p class='text-center comentarios-cerrados'>Los comentarios estan cerrados</p>";
+                endif;
+
+            ?>
+        </div>
     
-    endwhile; ?>
+    <?php endwhile; ?>
 
 <?php get_footer();?>
