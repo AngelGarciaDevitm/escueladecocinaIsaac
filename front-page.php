4efe8603cd1ba7af8678101de69f46456808f709
Angel Isaac Garcia <angel.garcia@devitm.com>
2025-07-04 09:23:20 -0600

    64-Mostrando los iconos en la pagina principal

--- front-page.php
+++ front-page.php
@@ -32,36 +32,13 @@
         </div><!--.row-->
     </div><!--.container-->
 
-    <div class="container">
-        <section class="nosotros mt-5">
-            <h2 class="text-center mb-5 separador">¿Porqué estudiar con nosotros?</h2>
-
-            <div class="row">
-                <div class="col-md-4 text-center informacion">
-                    <img src="img/icono_chef.png" alt="icono chef" class="img-fluid mb-3">
-                    <h3>
-                        Chef's Especialitas
-                    </h3>
-                    <p>Los mejores chef's que han trabajado en los mejores restaurantes de Europa.</p>
-                </div><!--col-md-4-->
-                <div class="col-md-4 text-center informacion">
-                    <img src="img/icono_vino.png" alt="icono vino" class="img-fluid mb-3">
-                    <h3>
-                        Incluye todo sobre bebidas
-                    </h3>
-                    <p>Además de la cocina, incluimos en el programa todo lo relacionado a bebidas.</p>
-                </div><!--col-md-4-->
-                <div class="col-md-4 text-center informacion">
-                    <img src="img/icono_menu.png" alt="icono menu" class="img-fluid mb-3">
-                    <h3>
-                        Siempre Actualizados
-                    </h3>
-                    <p>El programa se actualiza constantemente con nuevos platillos y técnicas</p>
-                </div><!--col-md-4-->
-            </div>
-        </section>
-    </div>
-
+  
+    <?php 
+    $nosotros = new WP_Query('pagename=nosotros');
+    while($nosotros->have_posts() ): $nosotros->the_post();
+            get_template_part('template', 'parts/iconos');
+    endwhile; wp_reset_postdata();
+    ?>
 
     <section class="clases mt-5 py-5">
         <h1 class="separador text-center mb-3">Próximas Clases</h1>
