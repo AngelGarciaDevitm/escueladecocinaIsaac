2093a139aa5262f2de4ff8c713d313e621d37ed6
Angel Isaac Garcia <angel.garcia@devitm.com>
2025-07-04 10:26:26 -0600

    67-Finalizando el Loop para mostrar las entradas

--- index.php
+++ index.php
@@ -28,75 +28,23 @@
                             </a>
                             <div class="meta pt-2 pt-md-0">
                                 <p class="m-0">
-                                    Escrito el: <span>20/10/24</span> por <a href="#">Admin</a>
+                                    Escrito el: <span><?php the_time( get_option('date_format') ); ?>
+                                             </span> 
+                                     por 
+                                     <a href="<?php 
+                                                echo get_author_posts_url(get_the_author_meta('ID'), get_the_author_meta('user_nicename') ); ?>" ><?php the_author(); ?>
+                                    </a>
                                 </p>
                             </div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae enim quibusdam, hic saepe ullam eveniet facilis eligendi earum beatae dolore et omnis qui quos blanditiis dolor vitae inventore ut voluptates?</p>
-                            <a href="entrada.html" class="btn btn-primary text-light">Ver Entrada</a>
+                            <p> <?php the_excerpt(); ?> </p>
+                            <a href="<?php the_permalink(); ?>" class="btn btn-primary text-light">Ver Entrada</a>
                         </div><!--contenido entrada-->
                     </div>
                 </div><!--.row-->
                 <?php endwhile; ?>
             </main>
 
-            <aside class="col-lg-3 col-md-4 bg-primary p-4">
-                <div class="proximos-cursos">
-                    <h2 class="text-center text-light separador inverso">Próximos Cursos</h2>
-
-                    <div class="card mb-4">
-                        <img src="img/clase1.jpg" alt="imagen de curso" class="img-fluid">
-                        <div class="card-body">
-                            <h3 class="card-title">Comida Italiana para principiantes
-                            </h3>
-                            <p class="card-subtitle m-0">
-                                Aprende las bases de la comida italiana con este curso
-                            </p>
-                        </div>
-                        <div class="card-footer">
-                            <a href="#">Más informacion</a>
-                        </div>
-                    </div>
-
-                    <div class="card mb-4">
-                        <img src="img/clase1.jpg" alt="imagen de curso" class="img-fluid">
-                        <div class="card-body">
-                            <h3 class="card-title">Comida Italiana para principiantes
-                            </h3>
-                            <p class="card-subtitle m-0">
-                                Aprende las bases de la comida italiana con este curso
-                            </p>
-                        </div>
-                        <div class="card-footer">
-                            <a href="#">Más informacion</a>
-                        </div>
-                    </div>
-                </div><!--proximos cursos-->
-
-                <div class="ultimas-entradas mt-5">
-                    <h2 class="text-center text-light">Últimas entradas</h2>
-
-                    <ul class="list-unstyled mt-4">
-                        <li>
-                            <a href="#">Titulo Entrada Texto</a>
-                        </li>
-                        <li>
-                            <a href="#">Titulo Entrada Texto</a>
-                        </li> 
-                        <li>
-                            <a href="#">Titulo Entrada Texto</a>
-                        </li>
-                         <li>
-                            <a href="#">Titulo Entrada Texto</a>
-                        </li>
-                         <li>
-                            <a href="#">Titulo Entrada Texto</a>
-                        </li>
-                         <li>
-                            <a href="#">Titulo Entrada Texto</a>
-                        </li> 
-                    </ul>
-                </div>
-            </aside>
+            <?php get_sidebar(); ?> 
         </div>
     </div>
 
